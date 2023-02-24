d1fe0fe60f45b8e736dc0b626f0f659d0e3784f6
Heine Günther <heine_gunther@hotmail.com>
2023-02-24 21:27:25 +0100

    Update like button

--- src/components/ReactionEmoji.js
+++ src/components/ReactionEmoji.js
@@ -1,41 +1,3 @@
-// import { useContext } from "react";
-// import { BASE_URL, POSTS_URL } from "../constants/api";
-// import { AuthContext } from "../context/AuthContext";
-// import { Button } from "@mui/material";
-
-// const url = BASE_URL + POSTS_URL;
-// const emoji = "❤️";
-
-// export default function ReactionEmoji({ postId }) {
-//   const { auth } = useContext(AuthContext);
-//   const { accessToken } = auth;
-
-//   const addLike = async () => {
-//     try {
-//       const response = await fetch(`${url}/${postId}/react/${emoji}`, {
-//         method: "PUT",
-//         headers: {
-//           Authorization: `Bearer ${accessToken}`,
-//         },
-//       });
-//       if (!response.ok) {
-//         throw new Error(`Error ${response.status}`);
-//       }
-//       const data = await response.json();
-//       console.log("Received data", data);
-//       this.setState({});
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   };
-
-//   return (
-//     <Button variant="outlined" onClick={addLike}>
-//       Like ❤️
-//     </Button>
-//   );
-// }
-
 import { useContext, useState } from "react";
 import { BASE_URL, POSTS_URL } from "../constants/api";
 import { AuthContext } from "../context/AuthContext";
@@ -47,7 +9,7 @@ const emoji = "❤️";
 export default function ReactionEmoji({ postId }) {
   const { auth } = useContext(AuthContext);
   const { accessToken } = auth;
-  const [refresh, setRefresh] = useState(false);
+  const [value, setValue] = useState();
 
   const addLike = async () => {
     try {
@@ -61,8 +23,7 @@ export default function ReactionEmoji({ postId }) {
         throw new Error(`Error ${response.status}`);
       }
       const data = await response.json();
-      console.log("Received data", data);
-      setRefresh(!refresh);
+      setValue({ data });
     } catch (error) {
       console.error(error);
     }
