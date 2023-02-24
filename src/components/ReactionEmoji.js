754ef56bc05f085dcf171b9a85d750779dd67253
Heine Günther <heine_gunther@hotmail.com>
2023-02-24 21:18:04 +0100

    update state like button

--- src/components/ReactionEmoji.js
+++ src/components/ReactionEmoji.js
@@ -23,7 +23,7 @@ export default function ReactionEmoji({ postId }) {
       }
       const data = await response.json();
       console.log("Received data", data);
-      window.location.reload();
+      this.setState({});
     } catch (error) {
       console.error(error);
     }
@@ -35,3 +35,42 @@ export default function ReactionEmoji({ postId }) {
     </Button>
   );
 }
+
+// import { useContext, useState } from "react";
+// import { BASE_URL, POSTS_URL } from "../constants/api";
+// import { AuthContext } from "../context/AuthContext";
+// import { Button } from "@mui/material";
+
+// const url = BASE_URL + POSTS_URL;
+// const emoji = "❤️";
+
+// export default function ReactionEmoji({ postId }) {
+//   const { auth } = useContext(AuthContext);
+//   const { accessToken } = auth;
+//   const [refresh, setRefresh] = useState(false);
+
+//   const addLike = async () => {
+//     try {
+//       const response = await fetch(`${url}/${postId}/react/${emoji}`, {
+//         method: "PUT",
+//         headers: {
+//           Authorization: `Bearer ${accessToken}`,
+//         },
+//       });
+//       if (!response.ok) {
+//         throw new Error(`Error ${response.status}`);
+//       }
+//       const data = await response.json();
+//       console.log("Received data", data);
+//       setRefresh(!refresh);
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   };
+
+//   return (
+//     <Button variant="outlined" onClick={addLike}>
+//       Like ❤️
+//     </Button>
+//   );
+// }
